62aa13f1cde0feac38628f9d3dc3b5ef216aeb98
tooooo1 <wer4272@nate.com>
2021-03-10 18:21:40 +0900

    study

--- basic100/basic_level_5-3/1096.py
+++ basic100/basic_level_5-3/1096.py
@@ -1,1 +1,18 @@
 n = int(input())
+location = []
+
+for i in range(20):
+       location.append([])
+
+        for j in range(20):
+             location[i].append(0)
+
+ for i in range(n):
+        x, y = map(int, input().split()) 
+        location[x][y] = 1
+        
+for i in range(1, 20):
+     for j in range(1, 20):
+          print(location[i][j], end=" ")
+          
+    print("")
